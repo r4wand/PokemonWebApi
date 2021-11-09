2d1d9d5ad654a2b452ab4a90f642160adef88e92
rawand <26229485+r4wand@users.noreply.github.com>
2021-11-09 11:23:34 +0300

    adds first migration

---
+++ Migrations/20211109080718_initial.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PokemonWebApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Abilities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Abilities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Breeds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Height = table.Column<double>(type: "double precision", nullable: false),
+                    EvolvedFromId = table.Column<int>(type: "integer", nullable: true),
+                    EvolvedToId = table.Column<int>(type: "integer", nullable: true),
+                    Image = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Breeds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Breeds_Breeds_EvolvedFromId",
+                        column: x => x.EvolvedFromId,
+                        principalTable: "Breeds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Breeds_Breeds_EvolvedToId",
+                        column: x => x.EvolvedToId,
+                        principalTable: "Breeds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Types", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbilityBreed",
+                columns: table => new
+                {
+                    AbilitiesId = table.Column<int>(type: "integer", nullable: false),
+                    BreedsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbilityBreed", x => new { x.AbilitiesId, x.BreedsId });
+                    table.ForeignKey(
+                        name: "FK_AbilityBreed_Abilities_AbilitiesId",
+                        column: x => x.AbilitiesId,
+                        principalTable: "Abilities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AbilityBreed_Breeds_BreedsId",
+                        column: x => x.BreedsId,
+                        principalTable: "Breeds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BreedType",
+                columns: table => new
+                {
+                    BreedsId = table.Column<int>(type: "integer", nullable: false),
+                    TypesId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BreedType", x => new { x.BreedsId, x.TypesId });
+                    table.ForeignKey(
+                        name: "FK_BreedType_Breeds_BreedsId",
+                        column: x => x.BreedsId,
+                        principalTable: "Breeds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BreedType_Types_TypesId",
+                        column: x => x.TypesId,
+                        principalTable: "Types",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbilityBreed_BreedsId",
+                table: "AbilityBreed",
+                column: "BreedsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Breeds_EvolvedFromId",
+                table: "Breeds",
+                column: "EvolvedFromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Breeds_EvolvedToId",
+                table: "Breeds",
+                column: "EvolvedToId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BreedType_TypesId",
+                table: "BreedType",
+                column: "TypesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbilityBreed");
+
+            migrationBuilder.DropTable(
+                name: "BreedType");
+
+            migrationBuilder.DropTable(
+                name: "Abilities");
+
+            migrationBuilder.DropTable(
+                name: "Breeds");
+
+            migrationBuilder.DropTable(
+                name: "Types");
+        }
+    }
+}
