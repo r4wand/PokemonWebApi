7a5b9688eb8934c27bc4e09cba933b4d393a58df
rawand <26229485+r4wand@users.noreply.github.com>
2021-11-09 11:22:48 +0300

    fixes data modeling

--- Models/Breed.cs
+++ Models/Breed.cs
@@ -1,12 +1,17 @@
 
 using System.Collections.Generic;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 namespace PokemonWebApi.Models
 {
     public class Breed
     {
         public int Id { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
         public List<Type> Types { get; set; }
@@ -16,9 +21,16 @@ namespace PokemonWebApi.Models
         public double Height { get; set; }
 
         // evolved from refrences the id of the previous breed
+        public int? EvolvedFromId { get; set; }
+
+
+        [ForeignKey("EvolvedFromId")]
         public Breed EvolvedFrom { get; set; }
 
         // evolved to refrences the id of the next breed
+        public int? EvolvedToId { get; set; }
+
+        [ForeignKey("EvolvedToId")]
         public Breed EvolvedTo { get; set; }
 
         // this is the image link of the breed  
